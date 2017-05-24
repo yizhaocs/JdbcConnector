e97fb6adfafe39c1476c176e99ec9ac6170d574b
yzhao <yi.zhao@adara.com>
2017-05-24 10:54:39 -0700

    1

---
+++ src/main/java/com/yizhao/com/Main.java
@@ -0,0 +1,10 @@
+package com.yizhao.com;
+
+/**
+ * Created by yzhao on 5/24/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        JdbcConnector.connectionTesting();
+    }
+}
